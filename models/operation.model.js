a3035c7f596a608982768b30217686ab03466821
skylerspaeth <skylerspaeth.scc@me.com>
2020-06-12 18:20:25 -0500

    update operation model

--- models/operation.model.js
+++ models/operation.model.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 module.exports.operationSchema = mongoose.Schema({
 	fcode: String,
 	fnum: Number,
+	origin: String,
+	destination: String,
 	aircraft: String,
 	depTime: String,
 	arrTime: String,
