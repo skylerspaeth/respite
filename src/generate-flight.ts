d08965eaba94b52db6a046899cbe8eb1c2d6d32d
skylerspaeth <skylerspaeth.scc@me.com>
2020-06-13 00:30:08 -0500

    generate a test flight for database with same schema

--- src/generate-flight.ts
+++ src/generate-flight.ts
@@ -2,6 +2,17 @@ const request = require('request-promise');
 const airlines = ["BAW", "AAL", "SWA", "DAL", "AFR", "QFA", "VIR", "SQC"];
 const count: number = parseInt(process.argv[2]) || 2
 
+interface Operation {
+	fcode: string;
+	fnum: string;
+	origin: string;
+	destination: string;
+	aircraft: string;
+	depTime: string;
+	arrTime: string;
+	classes: string[];
+}
+
 if (count as number % 2 != 0) {
 	console.log('Number of flights to be generated must be even to split across round trip flights generated.');
 	process.exit(1);
@@ -10,13 +21,24 @@ if (count as number % 2 != 0) {
 const airline = airlines[Math.floor(Math.random() * airlines.length)];
 
 (async() => {
-	const html: string = await request(`https://www.random.org/clock-times/?num=${count}&earliest=00%3A00&latest=23%3A59&interval=5&format=plain&rnd=new`, {}, (err, res, body) => {
+	let html: string = await request(`https://www.random.org/clock-times/?num=2&earliest=00%3A00&latest=23%3A59&interval=5&format=plain&rnd=new`, (err, res, body) => {
 		if (err) { console.log('API error:' + err) }
 		else {
 			return body;
 		}
-	})
-	const times: string[] = html.split('\n').filter(e => e != "" );
-	console.log(times);
+	});
+
+	let newOperation: Operation = {
+		fcode: airlines[Math.floor(Math.random() * airlines.length)],
+		fnum: Math.floor(Math.random() * 1000).toString(),
+		origin: "KAUS",
+		destination: "EGLL",
+		aircraft: "Airbus A350",
+		depTime: html.split('\n').filter(e => e != "" )[0],
+		arrTime: html.split('\n').filter(e => e != "" )[1],
+		classes: ["A", "B", "C"]
+	}
+//	const times: string[] = html.split('\n').filter(e => e != "" );
+	console.log(newOperation);
 })();
 
