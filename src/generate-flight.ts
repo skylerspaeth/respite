73c090b4c3600f01610e886345dfd091b86c078c
skylerspaeth <skylerspaeth.scc@me.com>
2020-06-13 02:47:18 -0500

    get flight generation fully functional

--- src/generate-flight.ts
+++ src/generate-flight.ts
@@ -1,4 +1,4 @@
-const count: number = parseInt(process.argv[2]) || 2
+const count: number = parseInt(process.argv[2]) || 4
 const request = require('request-promise');
 const
 	airlines = ["BAW", "AAL", "SWA", "DAL", "AFR", "QFA", "VIR", "SQC"],
@@ -8,7 +8,7 @@ const
 
 interface Operation {
 	fcode: string;
-	fnum: string;
+	fnum: number;
 	origin: string;
 	destination: string;
 	aircraft: string;
@@ -23,44 +23,44 @@ if (count as number % 2 != 0) {
 }
 
 (async() => {
-	let html: string = await request(`https://www.random.org/clock-times/?num=2&earliest=00%3A00&latest=23%3A59&interval=5&format=plain&rnd=new`,
+	let html: string = await request(`https://www.random.org/clock-times/?num=${count}&earliest=00%3A00&latest=23%3A59&interval=5&format=plain&rnd=new`,
 		(err, res, body) => {
 			if (err) { console.log('API error:' + err) }
-			else {
-				return body;
-			}
+			else { return body }
 		}
 	);
 
 	let origin = airports[Math.floor(Math.random() * airports.length)];
-	console.log('Origin: ' + origin);
 	function preventDup(originAirport) {
 		let newAirport;
-		do { newAirport = Math.floor(Math.random() * airports.length); console.log('trying again');
+		do { newAirport = Math.floor(Math.random() * airports.length);
 		} while (airports[newAirport] === originAirport);
 		return airports[newAirport];
 	}
-	/* function preventDup(originAirport) {
-		if (originAirport === airports[Math.floor(Math.random() * airports.length)]) {
-			console.log(`Origin airport: ${originAirport} is the same as randomly chosen destination of ${_}. Attempting again`);
-			preventDup();
-		} else {
-			console.log(`${originAirport} != ${_}, continuing...`);
-		}
-	}
-	preventDup(origin); */
-	console.log('Destination: ' + preventDup(origin));
 
-	let newOperation: Operation = {
+	let outgoingFlight: Operation = {
 		fcode: airlines[Math.floor(Math.random() * airlines.length)],
-		fnum: Math.floor(Math.random() * 1000).toString(),
-		origin: "KAUS",
-		destination: "EGLL",
-		aircraft: "Airbus A350",
+		fnum: Math.floor(Math.random() * 1000),
+		origin: origin,
+		destination: preventDup(origin),
+		aircraft: aircraft[Math.floor(Math.random() * aircraft.length)],
 		depTime: html.split('\n').filter(e => e != "" )[0],
 		arrTime: html.split('\n').filter(e => e != "" )[1],
 		classes: ["A", "B", "C"]
 	}
-	console.log(newOperation);
+	console.log(outgoingFlight);
+	function generateIncoming(from) {
+		return new Object({
+			fcode: from.fcode,
+			fnum: from.fnum += 1,
+			origin: from.destination,
+			destination: from.origin,
+			aircraft: from.aircraft,
+			depTime: html.split('\n').filter(e => e != "" )[2],
+			arrTime: html.split('\n').filter(e => e != "" )[3],
+			classes: from.classes
+		});
+	}
+	console.log(generateIncoming(outgoingFlight));
 })();
 
