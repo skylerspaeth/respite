a3583130f578ee84eec8da074f33794957541521
skylerspaeth <skylerspaeth.scc@me.com>
2020-06-12 23:56:10 -0500

    statically type generate-flight

--- generate-flight.ts
+++ generate-flight.ts
@@ -10,12 +10,13 @@ if (count as number % 2 != 0) {
 const airline = airlines[Math.floor(Math.random() * airlines.length)];
 
 (async() => {
-	const times: string[] = await request(`https://www.random.org/clock-times/?num=${count}&earliest=00%3A00&latest=23%3A59&interval=5&format=plain&rnd=new`, {}, (err, res, body) => {
+	const html: string = await request(`https://www.random.org/clock-times/?num=${count}&earliest=00%3A00&latest=23%3A59&interval=5&format=plain&rnd=new`, {}, (err, res, body) => {
 		if (err) { console.log('API error:' + err) }
 		else {
-			return body.split('\n').filter(e => e != "");
+			return body;
 		}
-	});
+	})
+	const times: string[] = html.split('\n').filter(e => e != "" );
 	console.log(times);
 })();
 
