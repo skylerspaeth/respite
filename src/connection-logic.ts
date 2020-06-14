939a12e40ab4bbbec66208ac010c800191e07213
skylerspaeth <skylerspaeth.scc@me.com>
2020-06-13 22:51:37 -0500

    make connection-logic read entire collection

--- src/connection-logic.ts
+++ src/connection-logic.ts
@@ -1,7 +1,20 @@
-const mongoose = require('mongoose');
-
-export default {
-	getItinerariesByICAO: (options) => {
+const
+// Database
+mongoose = require('mongoose'),
+	dbName: string = "respiteDB",
+	dbUrl: string = `mongodb://localhost:27017/${dbName}`,
+	{ operationSchema } = require('../models/operation.model.js'),
+	Operation = mongoose.model("Operation", operationSchema)
+;
+(async() => {
+	await mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+		if (!err) { console.log(`connected to mongoDB @ ${dbUrl}`) }
+		else { console.log('DB error:' + err) }
+	});
+	async function getItinerariesByICAO(options) {
 		// ... return flight objects that make a connected route
+		await Operation.find({}, (err, docs) => { console.log(err ? err : docs) });
 	}
-}
+	getItinerariesByICAO('yeet');
+})();
+
