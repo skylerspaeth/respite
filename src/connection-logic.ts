3084ed355373eb6ec27a60326ad4b648bd29d700
skylerspaeth <skylerspaeth.scc@me.com>
2020-08-07 20:45:44 -0500

    individually print itineraries

--- src/connection-logic.ts
+++ src/connection-logic.ts
@@ -1,6 +1,5 @@
 export {};
 const
-	// Database
 	mongoose = require('mongoose'),
 	dbName: string = "respiteDB",
 	dbUrl: string = `mongodb://localhost:27017/${dbName}`,
@@ -32,17 +31,21 @@ const
 				sroFlights.forEach((e) => {
 					console.log(`${e.origin} -> ${e.destination}`);
 				});
-				*/
+				 */
 				// note to self: when recursing through airports, ignore origin airport
 				let connectingFlights = sroFlights.filter((e) => {
 					if (e.destination == destination) {
-						console.log(`${chalk.green('Connection itinerary found:')} ${originServices[i]["origin"]} -> ${originServices[i]["destination"]}; ${e.origin} -> ${e.destination}:`);
-						console.log(originServices[i]);
+						//console.log(`${chalk.green('Connection itinerary found:')} ${originServices[i]["origin"]} -> ${originServices[i]["destination"]}; ${e.origin} -> ${e.destination}:`);
+						//console.log(originServices[i]);
 						return true;
 					}
 				});
 				// let sroFlights = await Operation.find({ origin: originServices[i]["destination"] }, (err, docs) => docs);
-				if (connectingFlights.length != 0) console.log(connectingFlights);
+				if (connectingFlights.length != 0) connectingFlights.forEach((c) => {
+					console.log(`${chalk.green('Connection itinerary found:')} ${originServices[i]["origin"]} -> ${originServices[i]["destination"]}; e.origin, e.destination go here`);
+					console.log(originServices[i]);
+					console.log(c);
+				});
 			}
 			return true;
 		} else { return false; }
@@ -56,10 +59,10 @@ const
 		else { return false; }
 	}
 	//getItinerariesByICAO('yeet');
-	const nonstops = await routeHasNonstop('KAUS','KORD');
+	const nonstops = await routeHasNonstop('KAUS','KDFW');
 	if (nonstops) { console.log(chalk.green(nonstops.length > 1 ? 'Nonstops exist: ' : 'Nonstop exists: '), nonstops) } else { console.log(chalk.yellow('No nonstops exists.')) }
 
-	const connections = await generateConnections('KAUS', 'KORD');
+	const connections = await generateConnections('KAUS', 'KDFW');
 	// if (connections) { console.log(connections) }
 	console.log(connections);
 
