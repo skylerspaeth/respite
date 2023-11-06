45271c3ada815f6f0ff0814fb10d6255c3ddd000
xero-lib <jonathankurtz@protonmail.com>
2023-11-06 15:33:09 -0600

    index: fixed indentation, removed extraneous async block

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const
-  // Database
-  dbName = 'respiteDB',
+	// Database
+	dbName = 'respiteDB',
 	dbUrl = `mongodb://localhost:27017/${dbName}`,
 	mongoose = require('mongoose'),
 	schemas = ["Operation"],
@@ -21,18 +21,16 @@ schemas.forEach((e) => {
 	global[e] = mongoose.model(e, global[schemaName]);
 });
 
-(async() => {
-	// Connect to mongoDB
-	await mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
-		if (!err) { console.log(`connected to mongoDB @ ${dbUrl}`) }
-		else { console.log('DB error:' + err) }
-	});
+// Connect to mongoDB
+await mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+if (!err) { console.log(`connected to mongoDB @ ${dbUrl}`) }
+	else { console.log('DB error:' + err) }
+});
 
-	const origin = prompt(chalk.cyan('Starting airport [KAUS]: ')).toUpperCase() || "KAUS";
-	const destination = prompt(chalk.cyan('Destination airport [KSFO]: ')).toUpperCase() || "KSFO";
-	const passclass = prompt(chalk.cyan('Passenger class [Y, b, p, j]: ')).toUpperCase() || "Y";
-	console.log(chalk.magenta(`From ${origin}; to ${destination}; in ${passclass} class`));
-})();
+const origin = prompt(chalk.cyan('Starting airport [KAUS]: ')).toUpperCase() || "KAUS";
+const destination = prompt(chalk.cyan('Destination airport [KSFO]: ')).toUpperCase() || "KSFO";
+const passclass = prompt(chalk.cyan('Passenger class [Y, b, p, j]: ')).toUpperCase() || "Y";
+console.log(chalk.magenta(`From ${origin}; to ${destination}; in ${passclass} class`));
 
 /* Databse test interaction stuff
 // Operation.findOne({ fcode: 'BAW' }, (err, doc) => { console.log(err ? err : doc) });
